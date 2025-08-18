b550b2441731dbcf24eb656944d43d7a575c1de7
Nuniyat Getamesay <nuniyat.getamesay-ug@aau.edu.et>
2025-08-18 13:01:00 +0300

    handling this undefined creating constuctor

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,12 +8,17 @@ class  Counter extends Component {
    
    }; 
 
+   constructor(){
+    super();
+    this.handleIncrement=this.handleIncrement.bind(this);
+   }
+
    renderTags(){
     if (this.state.tags.length===0)return <p>There are no tags!</p>;
    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
 }
 handleIncrement(){
-    console.log('increment Clicked');
+    console.log('increment Clicked',this);
 }
 
  
